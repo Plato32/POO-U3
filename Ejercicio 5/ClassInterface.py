de31676161535d166c025e1c6179435b9c40691e
Plato32 <99925500+Plato32@users.noreply.github.com>
2022-06-06 09:49:05 -0300

    Add files via upload

---
+++ Ejercicio 5/ClassInterface.py
@@ -0,0 +1,17 @@
+from zope.interface import Interface
+
+class Interfaz(Interface):
+    def insertarElemento(self, elem, pos):
+        pass
+
+    def agregarElemento(self, elem):
+        pass
+
+    def mostrarElemento(self, pos):
+        pass
+
+
+
+
+
+
